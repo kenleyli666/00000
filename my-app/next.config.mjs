9a7bc5d0a532e24963ae0503fe52dec6ea835210
kenleyli666 <107999510+kenleyli666@users.noreply.github.com>
2024-10-03 17:45:17 +0800

    Add files via upload

---
+++ my-app/next.config.mjs
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'fakeimg.pl',
+      },
+      {
+        protocol: 'https',
+        hostname: 'example.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'kenleyli666.github.io',
+      },
+    ],
+  }
+};
+
+export default nextConfig;
+
+  
